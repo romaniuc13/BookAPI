14ff59187d2f06d3161cc9c652780ac02d00fe0a
Romaniuc Maxim <romaniuc.maxim19@gmail.com>
2023-12-11 16:08:28 +0200

    This is a book application, made for the Java Tech Course at Endava!

---
+++ src/main/java/com/maxim/book/mappers/impl/BookMapperImpl.java
@@ -0,0 +1,27 @@
+package com.maxim.book.mappers.impl;
+
+import com.maxim.book.domain.dto.BookDto;
+import com.maxim.book.domain.entities.BookEntity;
+import com.maxim.book.mappers.Mapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookMapperImpl implements Mapper<BookEntity, BookDto> {
+
+    private ModelMapper modelMapper;
+
+    public BookMapperImpl(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public BookDto mapTo(BookEntity bookEntity) {
+        return modelMapper.map(bookEntity, BookDto.class);
+    }
+
+    @Override
+    public BookEntity mapFrom(BookDto bookDto) {
+        return modelMapper.map(bookDto, BookEntity.class);
+    }
+}
---
+++ src/main/java/com/maxim/book/repositories/BookRepository.java
@@ -0,0 +1,11 @@
+package com.maxim.book.repositories;
+
+import com.maxim.book.domain.entities.BookEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CrudRepository<BookEntity, String>,
+        PagingAndSortingRepository<BookEntity, String> {
+}
